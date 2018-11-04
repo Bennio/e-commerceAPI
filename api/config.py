c5f67ce8aef575494649281568375c0c2ff6c583
dengsang <dengsang@gmail.com>
2018-11-04 06:39:43 +0300

    update on the config file

--- api/config.py
+++ api/config.py
@@ -1,22 +1,19 @@
 import os
 import logging.config
-from flask import Flask
 from sqlalchemy.databases import postgres
-from api.models.models import app
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 # default config
-class BaseConfig(object):
-    app = Flask(__name__)
+class Config(object):
     DEBUG = False
     BCRYPT_LOG_ROUNDS = 13
-    app.config['TESTING'] = False
+    TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
     FLASK_SERVER_NAME = 'localhost:8000'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SWAGGER_UI_DOC_EXPANSION = 'list'
     RESTPLUS_VALIDATE = True
     RESTPLUS_MASK_SWAGGER = False
@@ -28,16 +25,20 @@ class BaseConfig(object):
     MAIL_USERNAME = os.environ.get('APP_MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('APP_MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = 'from@example.com'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=postgres,pw=postgres,url='jdbc:postgresql://localhost:5432/postgres',db=postgres)
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=postgres, pw=postgres,
+                                                                                    url='jdbc:postgresql://localhost:5432/postgres',
+                                                                                    db=postgres)
 
 
-class TestConfig(BaseConfig):
+class TestConfig(Config):
     DEBUG = True
     TESTING = True
     WTF_CSRF_ENABLED = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=postgres,pw=postgres,url='jdbc:postgresql"//localhost:5432/postgres',db=postgres)
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=postgres, pw=postgres,
+                                                                                    url='jdbc:postgresql"//localhost:5432/postgres',
+                                                                                    db=postgres)
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URI')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     PRESERVE_CONTEXT_ON_EXCEPTION = False
     BCRYPT_LOG_ROUNDS = 4
     PAYLOAD_EXPIRATION_TIME = 5
@@ -59,14 +60,22 @@ def configure_logging_relative(logging_ini):
     configure_logging(logging_ini)
 
 
-class DevelopmentConfig(BaseConfig):
+class DevelopmentConfig(Config):
     DEBUG = True
     DEVELOPMENT = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     PAYLOAD_EXPIRATION_TIME = 3000
 
 
-class ProductionConfig(BaseConfig):
+class ProductionConfig(Config):
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     PAYLOAD_EXPIRATION_TIME = 3000
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
